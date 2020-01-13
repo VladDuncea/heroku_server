c27dc93393c601a59d804560cbf7195d7763031f
Duncea Vlad <vlad_duncea_31@yahoo.com>
2020-01-13 17:55:37 +0200

    Update

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const port = 3000
 
 app.use(express.json())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, access-control-allow-origin")
-    next();
-  });
-
 app.get('/contact.php', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/contact.php', (req,res) => {
     console.log(req.body);
