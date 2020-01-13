ef6553978d9ab3110f5e76f08e9fbeb18fbe6f3f
Duncea Vlad <vlad_duncea_31@yahoo.com>
2020-01-13 21:44:21 +0200

    Update

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.json())
-
-app.get('/contact.php', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/contact.php', (req,res) => {
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .get('/', (req, res) => res.send('Hello Wordl!'))
+  .post('/contact.php', (req,res) => {
     console.log(req.body);
-    res.send({Status: 'OK'});
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    res.send({Status: 'OK'});})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
